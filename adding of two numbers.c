bc2a488df310b17eb7da556c5eca1a7efc5d6b9c
Mani1153 <92707790+Mani1153@users.noreply.github.com>
2021-10-20 10:10:42 +0530

    Add files via upload

---
+++ adding of two numbers.c
@@ -0,0 +1,14 @@
+/* Adding two numbers */
+#include<stdio.h>
+void main()
+{
+	//variable declaration 
+	int num1, num2, sum;
+	//initialization
+	num1= 10;
+	num2= 20;
+	//calculation
+	sum = num1 + num2;
+	// output 
+	printf("sum of %d and %d is %d", num1, num2,sum);
+}
