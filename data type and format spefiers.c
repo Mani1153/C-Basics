bd4c2521271f72d9a7f7f4fd0138a8b1d9bdbfd4
Mani1153 <92707790+Mani1153@users.noreply.github.com>
2021-10-20 10:46:10 +0530

    Add files via upload

---
+++ data type and format spefiers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	//variable declaration 
+	int age;
+	float cgpa;
+	char initial; 
+	//initialization
+	age = 20;
+	cgpa = 8.57;
+	initial = 'E';
+	// output 
+	printf("%d\n", age);
+	printf("%f\n", cgpa);
+	printf("%c", initial);
+}
